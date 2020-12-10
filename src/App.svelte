dc33d0ddbd2fb6a2586bf2a660a48e5ca4e26511
Akash Bhave <akashbbhave@gmail.com>
2020-12-10 09:39:02 -0500

    Update colors and fonts

--- src/App.svelte
+++ src/App.svelte
@@ -26,6 +26,19 @@
 </script>
 
 <style>
+  @font-face {
+    font-family: "Iosevka Curly Slab";
+    src: url("/fonts/iosevka-curly-slab-regular.woff2") format("woff2"),
+      url("/fonts/iosevka-curly-slab-regular.woff") format("woff");
+    font-weight: normal;
+  }
+  @font-face {
+    font-family: "Iosevka Curly Slab";
+    src: url("/fonts/iosevka-curly-slab-semibold.woff2") format("woff2"),
+      url("/fonts/iosevka-curly-slab-semibold.woff") format("woff");
+    font-weight: bold;
+  }
+
   main {
     height: 100%;
     width: 100%;
@@ -36,9 +49,14 @@
     justify-content: center;
 
     font-family: "Iosevka Curly Slab", monospace;
+    font-size: 2em;
+    font-weight: normal;
+
+    background-color: #dee5f0;
+    color: #183675;
   }
 </style>
 
 <main>
-  <h1>{timeString}</h1>
+  <p>{timeString}</p>
 </main>
